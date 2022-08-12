a298eca7dfd97b1cf870e8cfa0c1635be75fc020
Sean Garcy <swgarcy@gmail.com>
2022-08-12 09:13:13 -0600

    maybe this works

--- frontend/src/components/Editor.js
+++ frontend/src/components/Editor.js
@@ -121,7 +121,7 @@ class Editor extends React.Component {
                       className="form-control"
                       type="text"
                       placeholder="Image url"
-                      value='placeholder.png'
+                      value={this.props.image}
                       onChange={this.changeImage}
                     />
                   </fieldset>
